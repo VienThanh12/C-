e84c31ae36231aa400d2ecc7f6a585732f6e4e7e
Kevin <67015555+VienThanh12@users.noreply.github.com>
2023-04-24 22:44:38 +0300

    Create OOPAll_In_One.cpp

---
+++ OOPAll_In_One.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Book
+{
+private:
+    string title, author;
+    int year;
+public:
+    Book(string title, string author, int year)
+    {
+        this->title = title;
+        this->author = author;
+        this->year = year;
+    }
+
+    ~Book()
+    {
+        cout << "In book:" + title + " destructor" << endl;
+    }
+
+    string getData()
+    {
+        return "Title:" + title + " Author:" + author + " Year:" + to_string(year);
+    }
+};
+
+int main()
+{
+    Book *myBook = new Book("Winnie the Pooh", "Milne", 1950);
+    Book *otherBook = new Book("The world accordind to Garp", "Irwing", 1985);
+
+    //myBook->title = "new title"; // title is private member of class Book
+
+    cout << myBook->getData() << endl;
+    cout << otherBook->getData() << endl;
+
+    delete(myBook);
+    delete(otherBook);
+}
+
