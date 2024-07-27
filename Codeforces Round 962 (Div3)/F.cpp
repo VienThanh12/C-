2c0004a16a55b6c4922d953e56bcaf9fe892de26
VienThanh <vienha000@gmail.com>
2024-07-27 14:04:17 +0300

    Codeforces Round 962 (Div3)

--- Codeforces Round 962 (Div3)/F.cpp
+++ Codeforces Round 962 (Div3)/F.cpp
@@ -1,47 +1,46 @@
-#include <iostream>
-#include <vector>
-#include <string>
-
+#include<iostream>
+#include<algorithm>
 using namespace std;
+typedef long long ll;
+#define For(type, i, a, b) for (type i = (a); i <= (b); ++i)
+#define endl '\n'
 
-const int MOD = 1e9 + 7;
+ll test;
+ll n, x;
+string s;
+int main(){
+    cin >> test;
+    while(test--){
+        ll cnt_0 = 0, cnt_1 = 0;
+        ll res = 0, ans = 0, real_ans = 0;
+        cin >> s;
+        if(s[0] == '0') cnt_0 ++;
+        if(s[0] == '1') cnt_1 ++;
 
-int main() {
-    int testCases;
-    cin >> testCases;
-
-    while (testCases--) {
-        string binaryString;
-        cin >> binaryString;
-
-        int n = binaryString.size();
-        vector<int> prefixSum(n + 1);
-        vector<int> count(2 * n + 2);
-
-        // Calculate prefix sum
-        for (int i = 0; i < n; i++) {
-            prefixSum[i + 1] = prefixSum[i] + (binaryString[i] == '1' ? 1 : -1);
+        For(ll, i, 1, s.length() - 1){
+            if(cnt_0 > cnt_1){
+                if(s[i] == '1'){
+                    if(ans == 0) ans = 1;
+                    else ans = ans * 2;
+                }
+            }
+            else if(cnt_0 < cnt_1){
+                if(s[i] == '0'){
+                    if(ans == 0) ans = 1;
+                    else ans = ans * 2;
+                }
+            }
+            else if(cnt_0 == cnt_1){
+                if(s[i] != s[i - 1]){
+                    if(ans == 0) ans = 1;
+                    else ans = ans * 2;
+                }
+            }
+            cout << ans << endl;
+            res += ans;
+            if(s[i] == '0') cnt_0 ++;
+            if(s[i] == '1') cnt_1 ++;
         }
-        for(int i = 0; i < n; i++)
-            cout << prefixSum[i] << " ";
-            
-
-        // Initialize count array
-        for (int i = 0; i < 2 * n + 2; i++) {
-            count[i] = 0;
-        }
-
-        long long answer = 0;
-        for (int j = 1; j <= n; j++) {
-            // Update count array
-            count[prefixSum[j - 1] + n] += j;
-
-            // Calculate answer
-            answer = (answer + (long long)count[prefixSum[j] + n] * (n - j + 1)) % MOD;
-        }
-
-        cout << answer << endl;
+        cout << res << endl;
     }
-
-    return 0;
 }
