eaad3f02a55e8e76e102654b35ff35569741a8b0
VienThanh <vienha000@gmail.com>
2024-07-27 18:04:52 +0300

    Codeforces Round 962 (Div3)

--- Codeforces Round 962 (Div3)/E.cpp
+++ Codeforces Round 962 (Div3)/E.cpp
@@ -1,46 +1,43 @@
 #include<iostream>
 #include<algorithm>
+#include<map>
 using namespace std;
 typedef long long ll;
 #define For(type, i, a, b) for (type i = (a); i <= (b); ++i)
 #define endl '\n'
 
 ll test;
-ll n, x;
+ll n, m = 1e9 + 7;
+const ll limit = 2e5 + 10;
+ 
+ll prefix[limit];
+ll a[limit];
+
+map<ll, ll> sum_x;
 string s;
+
 int main(){
     cin >> test;
     while(test--){
-        ll cnt_0 = 0, cnt_1 = 0;
-        ll res = 0, ans = 0, real_ans = 0;
         cin >> s;
-        if(s[0] == '0') cnt_0 ++;
-        if(s[0] == '1') cnt_1 ++;
-
-        For(ll, i, 1, s.length() - 1){
-            if(cnt_0 > cnt_1){
-                if(s[i] == '1'){
-                    if(ans == 0) ans = 1;
-                    else ans = ans * 2;
-                }
-            }
-            else if(cnt_0 < cnt_1){
-                if(s[i] == '0'){
-                    if(ans == 0) ans = 1;
-                    else ans = ans * 2;
-                }
-            }
-            else if(cnt_0 == cnt_1){
-                if(s[i] != s[i - 1]){
-                    if(ans == 0) ans = 1;
-                    else ans = ans * 2;
-                }
-            }
-            cout << ans << endl;
-            res += ans;
-            if(s[i] == '0') cnt_0 ++;
-            if(s[i] == '1') cnt_1 ++;
+        prefix[0] = 0;
+        For(ll, i, 0, s.length() - 1){
+            a[i] = (s[i] == '0' ? 1 : -1);
+            prefix[i + 1] = prefix[i] + a[i];
         }
-        cout << res << endl;
+        int ans = 0;
+        For(ll, i, 0, s.length()){
+            // cout << sum_x[prefix[i]] << endl;
+            ans = (ans % m + ((s.length() - i + 1) % m * sum_x[prefix[i]] % m) % m) % m;
+            sum_x[prefix[i]] = (sum_x[prefix[i]] % m + (i + 1) % m) % m;
+        }
+        cout << ans << endl;
+        For(ll, i, 0, s.length()){
+            a[i] = 0;
+            sum_x[prefix[i]] = 0;
+            prefix[i] = 0;
+        }
+        For(ll, i, -s.length() - 1, s.length() + 1)
+            sum_x[i] = 0;
     }
 }
