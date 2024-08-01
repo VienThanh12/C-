f9973e97c02684dfe87710603d1362d419bbe12e
VienThanh <vienha000@gmail.com>
2024-08-01 10:50:21 +0300

    Educational Codeforces Round 168 (Div 2)

--- Educational Codeforces Round 168 (Div2)/B.cpp
+++ Educational Codeforces Round 168 (Div2)/B.cpp
@@ -8,10 +8,22 @@ typedef long long ll;
 ll res = 0;
 ll test;
 ll n;
+string a, b;
 
 int main(){
     cin >> test;
     while(test--){
         cin >> n;
+        cin >> a;
+        cin >> b;
+        ll res = 0;
+        For(ll, i, 0, n - 2){
+            if(a[i] == '.' && a[i + 1] == '.' && a[i + 2] == '.' && b[i] == 'x' && b[i + 1] == '.' && b[i + 2] == 'x')
+                res++;
+            
+            if(b[i] == '.' && b[i + 1] == '.' && b[i + 2] == '.' && a[i] == 'x' && a[i + 1] == '.' && a[i + 2] == 'x')
+                res++;
+        }
+        cout << res << endl;
     }
 }
