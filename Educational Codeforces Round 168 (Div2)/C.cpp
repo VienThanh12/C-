3433c3f52e96e52190d5a2468b70f1952f9b2101
VienThanh <vienha000@gmail.com>
2024-08-02 11:05:14 +0300

    Educational Codeforces Round 168 (Div2)

--- Educational Codeforces Round 168 (Div2)/C.cpp
+++ Educational Codeforces Round 168 (Div2)/C.cpp
@@ -8,10 +8,29 @@ typedef long long ll;
 ll res = 0;
 ll test;
 ll n;
+const ll limit = 2e5 + 10;
+string s;
+ll a[limit];
 
 int main(){
     cin >> test;
     while(test--){
         cin >> n;
+        cin >> s;
+        For(ll, i, 1, n){
+            if(s[i - 1] == '_') a[i] = 0;
+            if(s[i - 1] == '(' ) a[i] = 1;
+            if(s[i - 1] == ')') a[i] = -1;
+        }
+        ll res = 0;
+        for(ll i = 1; i <= n; i += 2){
+            if(a[i + 1] == -1){
+                res ++;
+            }
+            if(a[i + 1] == 1){
+                res += 3;
+            }
+        }
+        cout << res << endl;
     }
 }
