58d50fff7cdfe773a8965c9fd2b3f0d0488a486d
VienThanh <vienha000@gmail.com>
2024-08-17 14:40:03 +0300

    Educational Codeforces Round 169 (Div 2)

--- Educational Codeforces Round 169 (Rated for Div. 2)/B.cpp
+++ Educational Codeforces Round 169 (Rated for Div. 2)/B.cpp
@@ -1,14 +1,24 @@
 #include <iostream>
 #include<algorithm>
 using namespace std;
-
+ 
 typedef long long ll;
 #define For(type, i, a, b) for (type i = (a); i <= (b); ++i)
 #define endl '\n'
  
-
+ll x, y, u, v;
+ 
 void solve() {
-	
+	cin >> x >> y >> u >> v;
+	ll res = 0;
+
+	if(x > u || x < u) res++;
+	if(y < v || y > v) res++;
+ 
+	if(min(y, v) - max(x, u) > 0){
+		res += min(y, v) - max(x, u);
+	}
+	cout << res << endl;
 }
  
 int main() {
