5b823761c21e5f74d07d2891ea2400dc02ffaa58
VienThanh <vienha000@gmail.com>
2024-09-05 00:57:38 +0300

    Codeforces Round 971 (Div 3)

--- Codeforces Round 971 (Div. 4)/E.cpp
+++ Codeforces Round 971 (Div. 4)/E.cpp
@@ -9,9 +9,8 @@ typedef long long ll;
 ll n, k;
 
 
-ll cal(ll i, ll j){
-    ll ssh = j - i + 1;
-    return ssh * (i + j) / 2;
+ll cal(ll x, ll y) {
+    return y * (y + 1) / 2 - x * (x - 1) / 2;
 }
 void solve() {
     cin >> n >> k;
@@ -19,29 +18,15 @@ void solve() {
     ll diff = 1e18;
     ll res = 1e18;
     ll first = start, second = end;
-
     while(first <= second){
         mid = (first + second) / 2;
-        ll temp = abs(cal(start, mid) - cal(mid + 1, end));
-        res = min(res, temp);
-        cout << mid << endl;
-        if(temp <= diff){
-            diff = temp;
-            For(ll, i, -1000, 1000){
-                ll kd = mid + i;
-                if(start <= kd && kd <= end){
-                    res = min(res, abs(cal(start, kd) - cal(kd + 1, end)));
-                }
-            }
+        ll temp = cal(start, mid) - cal(mid + 1, end);
+        res = min(res, abs(temp));
+
+        if(temp <= 0){
             first = mid + 1;
         }
         else {
-            For(ll, i, -1000, 1000){
-                ll kd = mid + i;
-                if(start <= kd && kd <= end){
-                    res = min(res, abs(cal(start, kd) - cal(kd + 1, end)));
-                }
-            }
             second = mid - 1;
         }
     }
