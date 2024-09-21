11b5c76286e35b74abd044827aaa6b1f86a5d012
VienThanh <vienha000@gmail.com>
2024-09-21 12:45:55 +0300

    Codeforces Round 944 (Div 4)

--- Codeforces Round 944 (Div. 4)/D.cpp
+++ Codeforces Round 944 (Div. 4)/D.cpp
@@ -1,3 +1,19 @@
+/*
+https://codeforces.com/contest/1971/problem/D
+the idea of this problem is kinda interesting
+
+You are given a binary string†. Please find the minimum number of pieces you need to cut it into, so that the resulting pieces can be rearranged into a sorted binary string.
+
+
+Note that:
+
+each character must lie in exactly one of the pieces;
+the pieces must be contiguous substrings of the original string;
+you must use all the pieces in the rearrangement.
+† A binary string is a string consisting of characters 𝟶 and 𝟷. A sorted binary string is a binary string such that all characters 𝟶 come before all characters 𝟷.
+
+
+*/
 #include <iostream>
 #include<algorithm>
 using namespace std;
@@ -16,8 +32,14 @@ void solve() {
         return;
     }
     For(ll, i, 1, s.length() - 1){
-        if(s[i - 1] > s[i])
+        if(s[i - 1] != s[i])
             res++;
+    }
+    For(ll, i, 1, s.length() - 1){
+        if(s[i - 1] == '0' && s[i] == '1'){
+            res --;
+            break;
+        }
     }
     cout << res << endl;
 }
