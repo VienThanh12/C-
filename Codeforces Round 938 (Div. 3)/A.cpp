68dd382db2e2b8a3a8b57879449e8e8269f81ed8
VienThanh <vienha000@gmail.com>
2024-10-02 19:14:44 +0300

    Codeforces Round 938 (Div. 3)

---
+++ Codeforces Round 938 (Div. 3)/A.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include<algorithm>
+using namespace std;
+
+typedef long long ll;
+#define For(type, i, a, b) for (type i = (a); i <= (b); ++i)
+#define endl '\n'
+ 
+
+void solve() {
+	ll n, a, b;
+    cin >> n >> a >> b;
+
+    cout << min(a * n, n / 2 * b + n % 2 * a) << endl;
+}
+ 
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+	int tt; cin >> tt; for (int i = 1; i <= tt; i++) {solve();}
+}
