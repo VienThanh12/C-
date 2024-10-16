6b0ff4686d23975a313006e9f6e6133bdb94af0b
VienThanh <vienha000@gmail.com>
2024-10-17 02:33:52 +0300

    Codeforces Round 918 (Div. 4)

--- Codeforces Round 918 (Div. 4)/A.cpp
+++ Codeforces Round 918 (Div. 4)/A.cpp
@@ -8,11 +8,17 @@ typedef long long ll;
 #define NO cout << "NO\n"
 
 ll ans = 0, res = 0, temp = 0;
-ll a, b, c;
+ll a[5];
 
 void solve() {
-	cin >> a >> b >> c;
-    cout << min(a, min(b, c)) << endl;
+	cin >> a[1] >> a[2] >> a[3];
+    sort(a + 1, a + 1 + 3);
+
+    if(a[1] != a[2]){
+        cout << a[1] << endl;
+        return;
+    }
+    else cout << a[3] << endl;
 }
  
 int main() {
