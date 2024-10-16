c7a8ccb78ec66e87ea5ed4c8d7fea979b29ec628
VienThanh <vienha000@gmail.com>
2024-10-17 02:31:23 +0300

    Codeforces Round 918 (Div 4)

---
+++ Codeforces Round 918 (Div. 4)/A.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include<algorithm>
+using namespace std;
+typedef long long ll;
+#define For(type, i, a, b) for (type i = (a); i <= (b); ++i)
+#define endl '\n'
+#define YES cout << "YES\n"
+#define NO cout << "NO\n"
+
+ll ans = 0, res = 0, temp = 0;
+ll a, b, c;
+
+void solve() {
+	cin >> a >> b >> c;
+    cout << min(a, min(b, c)) << endl;
+}
+ 
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+	int tt; cin >> tt; for (int i = 1; i <= tt; i++) {solve();}
+}
