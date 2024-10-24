b8baa0a8d10faa0349c040f536fc845f2062db94
VienThanh <vienha000@gmail.com>
2024-10-25 02:10:05 +0300

    Codeforces Round 981 (Div. 3)

---
+++ Codeforces Round 981 (Div 3)/A.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include<algorithm>
+using namespace std;
+typedef long long ll;
+#define For(type, i, a, b) for (type i = (a); i <= (b); ++i)
+#define endl '\n'
+#define YES cout << "YES\n"
+#define NO cout << "NO\n"
+
+ll ans = 0, res = 0, temp = 0;
+ll n;
+
+void solve() {
+	cin >> n;
+    ll x = ((n + 1) % 2);
+    if(x % 2 == 0){
+        cout << "Kosuke" << endl;
+    }
+    else cout <<"Sakurako" << endl;
+}
+ 
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+	int tt; cin >> tt; for (int i = 1; i <= tt; i++) {solve();}
+}
