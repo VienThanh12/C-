771fdd3a735c5e707057aa65aa1e038f9f1f1fbd
VienThanh <vienha000@gmail.com>
2024-11-24 00:41:44 +0200

    CodeTON Round 9 (Div. 1 + Div. 2, Rated, Prizes!)

---
+++ CodeTON Round 9 (Div. 1 + Div. 2, Rated, Prizes!)/A.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include<algorithm>
+using namespace std;
+typedef long long ll;
+#define For(type, i, a, b) for (type i = (a); i <= (b); ++i)
+#define endl '\n'
+#define YES cout << "YES\n"
+#define NO cout << "NO\n"
+
+ll ans = 0, res = 0, temp = 0;
+ll n;
+
+void solve() {
+	cin >> n;
+    res = 1;
+    For(ll, i, 1, n){
+        cout << res <<" ";
+        res += 2;   
+    }
+    cout << endl;
+}
+ 
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+	int tt; cin >> tt; for (int i = 1; i <= tt; i++) {solve();}
+}
