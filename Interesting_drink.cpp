cbfa20ce489c2d6c825f2100c772ce839cd4d0da
VienThanh <vienha000@gmail.com>
2025-01-05 02:27:07 +0200

    706B - Interesting drink

---
+++ Interesting_drink.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include<algorithm>
+using namespace std;
+typedef long long ll;
+#define For(type, i, a, b) for (type i = (a); i <= (b); ++i)
+#define endl '\n'
+#define YES cout << "YES\n"
+#define NO cout << "NO\n"
+
+ll ans = 0, res = 0, temp = 0;
+ll n;
+const ll limit = 1e5 + 10;
+ll a[limit], dp[limit];
+ll q, x;
+void solve() {
+	cin >> n;
+    For(ll, i, 1, n){
+        cin >> a[i];
+    }
+    ll x = 1;
+    sort(a + 1, a + 1 + n);
+    For(ll, i, 1, 100000){
+        dp[i] = dp[i - 1];
+        if(i >= a[x]){
+            x++;
+            dp[i] ++;
+        }
+    }
+    cin >> q;
+    while(q--){
+        cin >> x;
+        cout << dp[x] << endl;
+    }
+    
+}
+ 
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+    solve();
+}
