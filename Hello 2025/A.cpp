3ff3b94df5c9eda70c7f1623e34f8e5bd750edd6
VienThanh <vienha000@gmail.com>
2025-01-06 03:21:57 +0200

    Hello 2025

---
+++ Hello 2025/A.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include<algorithm>
+using namespace std;
+typedef long long ll;
+#define For(type, i, a, b) for (type i = (a); i <= (b); ++i)
+#define endl '\n'
+#define YES cout << "YES\n"
+#define NO cout << "NO\n"
+
+ll ans = 0, res = 0, temp = 0;
+ll n, m;
+
+void solve() {
+	cin >> n >> m;
+	cout << max(n, m) + 1 << endl;
+}
+ 
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+	int tt; cin >> tt; for (int i = 1; i <= tt; i++) {solve();}
+}
