08050236f471bf7ca41aaced74e7824871f121a7
VienThanh <vienha000@gmail.com>
2025-01-20 00:38:39 +0200

    236A - Boy or Girl

---
+++ Codeforces Round 146 (Div. 2)/A.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include<algorithm>
+using namespace std;
+typedef long long ll;
+#define For(type, i, a, b) for (type i = (a); i <= (b); ++i)
+#define endl '\n'
+#define YES cout << "YES\n"
+#define NO cout << "NO\n"
+
+ll ans = 0, res = 0, temp = 0;
+ll n;
+string s;
+ll dp[200];
+
+void solve() {
+	cin >> s;
+
+    ll cnt = 0;
+    For(ll, i, 0, s.length() - 1){
+        if(dp[s[i]] == 0){
+            dp[s[i]]++;
+            cnt++;
+        } 
+    }
+    cnt % 2 == 0 ? cout <<"CHAT WITH HER!" : cout <<"IGNORE HIM!";
+}
+ 
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+    {solve();}
+}
