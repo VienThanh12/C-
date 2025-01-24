0dc68421238e4a4ba85454b8ad46e198a7de53e3
VienThanh <vienha000@gmail.com>
2025-01-25 01:30:50 +0200

    Codeforces Round 1000 (Div. 2)

---
+++ Codeforces Round 1000 (Div. 2)/A.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include<algorithm>
+using namespace std;
+typedef long long ll;
+#define For(type, i, a, b) for (type i = (a); i <= (b); ++i)
+#define endl '\n'
+#define YES cout << "YES\n"
+#define NO cout << "NO\n"
+
+ll ans = 0, res = 0, temp = 0;
+ll x, y;
+
+void solve() {
+	cin >> x >> y;
+    if(x == y && x == 1){
+        cout << 1 << endl;
+        return;
+    }
+    cout <<  y - x << endl;
+}
+ 
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+	int tt; cin >> tt; for (int i = 1; i <= tt; i++) {solve();}
+}
